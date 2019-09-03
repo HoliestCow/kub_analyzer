71301250da6c7cda9cb145bc655020e5b6b75add
HoliestCow <cbritt2@vols.utk.edu>
2019-09-03 17:07:25 -0400

    changed the index to pandas.Timestamp(). Added columns organizing things by hour, weekday, month, year, etc.

--- main.py
+++ main.py
@@ -32,10 +32,17 @@ def main():
         water_dataframe += [current_dataframe]
 
     electric_dataframe = pd.concat(electric_dataframe)
+    electric_dataframe = electric_dataframe.set_index('time')
     water_dataframe = pd.concat(water_dataframe)
+    water_dataframe = water_dataframe.set_index('time')
 
     master_dataframe = pd.merge(electric_dataframe, water_dataframe,
-                                on='time', how='inner')
+                                left_index=True, right_index=True)
+    master_dataframe['year'] = master_dataframe.index.year
+    master_dataframe['month'] = master_dataframe.index.month
+    master_dataframe['weekday_name'] = master_dataframe.index.weekday_name
+    master_dataframe['hour'] = master_dataframe.index.hour
+    print(master_dataframe.sample(5, random_state=0))
     return
 
 main()
